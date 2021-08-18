eced1a82a7ed057060f94250d7677ac7c351ad2a
Rainer Koirikivi <rainer@koirikivi.fi>
2021-08-18 13:13:15 +0300

    check_log_inconsistencies v2

--- check_log_inconsistencies.py
+++ check_log_inconsistencies.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 """
-Check for inconsistencies in log data returned by eth_getLogs and eth_getTransactionReceipt
-Note that this won't detect logs that are not returned by eth_getLogs.
+Check for inconsistencies in logs returned by eth_getLogs and eth_getTransactionReceipt
+
+- Check that the `data` field is the same for logs with same transactionHash and logIndex,
+  between JSONRPC responses from eth_getLogs and eth_getTransactionReceipt
+- Check that the logs from eth_getLogs are also found from eth_getTransactionReceipt
+- Check that the logs from eth_getTransactionReceipt are also found from eth_getLogs
+- Check that logs from eth_getLogs are not from failed transactions
+
+Note that this might not detect all missing logs from eth_getLogs.
 
 Run like this:
 
@@ -10,6 +17,7 @@ Run like this:
 Python 3.6 or later required. No external libraries required.
 """
 import argparse
+from collections import Counter
 import json
 from dataclasses import dataclass
 from typing import Any, Dict, List, Optional
@@ -34,7 +42,7 @@ def main():
     to_block = args.to_block
     while from_block <= to_block:
         batch_to_block = min(from_block + args.batch_size, to_block)
-        print(f"Checking blocks {from_block} to {batch_to_block}")
+        print(f"Checking blocks {from_block} to {batch_to_block} (up to {to_block})")
         logs = client.get_logs(from_block, batch_to_block)
         batch_inconsistencies = check_inconsistencies(
             client=client,
@@ -46,12 +54,17 @@ def main():
 
     if inconsistencies:
         print("\n\nDetected inconsistencies:")
+        type_counter = Counter()
         for inconsistency in inconsistencies:
+            type_counter[inconsistency.type] += 1
             print(inconsistency)
         print(
             f"Total of {len(inconsistencies)} inconsistencies detected between blocks "
             f"{args.from_block} and {args.to_block}."
         )
+        print("Inconsistencies by types:")
+        for inconsistency_type, count in type_counter.most_common():
+            print(inconsistency_type.ljust(45), count)
     else:
         print(f"No inconsistencies detected between blocks {args.from_block} and {args.to_block}.")
 
@@ -92,52 +105,22 @@ class JSONRPCClient:
         return response
 
 
-def check_inconsistencies(client: JSONRPCClient, logs: List[Dict[str, any]]):
-    logs_by_tx_hash_and_log_index = {}
-    tx_hashes = set()
-    for log in logs:
-        tx_hashes.add(log['transactionHash'])
-        logs_by_tx_hash_and_log_index[(log['transactionHash'], log['logIndex'])] = log
-
-    inconsistencies = []
-    print(f"{len(tx_hashes)} transactions")
-    for tx_hash in tx_hashes:
-        print(f"Checking {tx_hash}")
-        tx_receipt = client.get_transaction_receipt(tx_hash)
-        for receipt_log in tx_receipt['logs']:
-            log_key = (tx_hash, receipt_log['logIndex'])
-            try:
-                getlogs_log = logs_by_tx_hash_and_log_index[log_key]
-            except KeyError:
-                print(f"ERROR! Log {log_key} not found in eth_getLogs response!")
-                print("Receipt log:")
-                print(receipt_log)
-                continue
-            inconsistency = check_inconsistency(
-                receipt_log=receipt_log,
-                getlogs_log=getlogs_log,
-            )
-            if inconsistency:
-                print("INCONSISTENCY DETECTED!")
-                print(inconsistency)
-                inconsistencies.append(inconsistency)
-    return inconsistencies
-
-
 @dataclass
 class LogInconsistency:
     tx_hash: str
     log_index: int
     block_number: int
-    receipt_log: Dict[str, Any]
-    getlogs_log: Dict[str, Any]
+    receipt_log: Optional[Dict[str, Any]]
+    getlogs_log: Optional[Dict[str, Any]]
+    type: str
 
     def __repr__(self):
-        receipt_data = self.receipt_log['data']
-        getlogs_data = self.getlogs_log['data']
+        receipt_data = self.receipt_log['data'] if self.receipt_log else '(none)'
+        getlogs_data = self.getlogs_log['data'] if self.getlogs_log else '(none)'
         lines = [
             f"<INCONSISTENCY(",
             f"  block: {self.block_number}, tx: {self.tx_hash}, index: {self.log_index}",
+            f"  type: {self.type}",
             f"  receipt data: {receipt_data}",
             f"  getlogs data: {getlogs_data}",
             ")>",
@@ -145,16 +128,102 @@ class LogInconsistency:
         return '\n'.join(lines)
 
 
-def check_inconsistency(receipt_log: Dict[str, Any], getlogs_log: Dict[str, Any]) -> Optional[LogInconsistency]:
+def check_inconsistencies(client: JSONRPCClient, logs: List[Dict[str, any]]):
+    logs_by_tx_hash_and_log_index = {}
+    tx_hashes = set()
+    for log in logs:
+        tx_hashes.add(log['transactionHash'])
+        log_key = (log['transactionHash'], log['logIndex'])
+        assert log_key not in logs_by_tx_hash_and_log_index, f'{log_key} would be replaced'
+        logs_by_tx_hash_and_log_index[log_key] = log
+
+    failed_tx_hashes = set()
+    inconsistencies = []
+    print(f"{len(tx_hashes)} transactions")
+    for i, tx_hash in enumerate(tx_hashes, start=1):
+        # print(f"Checking {tx_hash}")
+        print_percentage_progress(i, len(tx_hashes))
+        tx_receipt = client.get_transaction_receipt(tx_hash)
+
+        if int(tx_receipt['status'], 16) == 0:
+            print(f"NOTE: Transaction {tx_hash} is failed, should not have logs")
+            failed_tx_hashes.add(tx_hash)
+            continue  # process these later
+
+        for receipt_log in tx_receipt['logs']:
+            log_key = (tx_hash, receipt_log['logIndex'])
+            try:
+                getlogs_log = logs_by_tx_hash_and_log_index[log_key]
+            except KeyError:
+                print(f"INCONSISTENCY DETECTED! Log {log_key} found in tx receipt but not in eth_getLogs response!")
+                inconsistencies.append(LogInconsistency(
+                    tx_hash=tx_hash,
+                    log_index=int(receipt_log['logIndex'], 16),
+                    block_number=int(receipt_log['blockNumber'], 16),
+                    type='log missing from eth_getLogs',
+                    receipt_log=receipt_log,
+                    getlogs_log=None
+                ))
+            else:
+                inconsistency = check_data_mismatch(
+                    receipt_log=receipt_log,
+                    getlogs_log=getlogs_log,
+                )
+                if inconsistency:
+                    print("INCONSISTENCY DETECTED -- data mismatch")
+                    print(inconsistency)
+                    inconsistencies.append(inconsistency)
+
+                # Delete this so we know the things that are left
+                del logs_by_tx_hash_and_log_index[log_key]
+
+    for failed_tx_hash in failed_tx_hashes:
+        log_keys = list(k for k in logs_by_tx_hash_and_log_index.keys() if k[0] == failed_tx_hash)
+        for log_key in log_keys:
+            print(f"INCONSISTENCY! Log {log_key} is from a failed transaction.")
+            getlogs_log = logs_by_tx_hash_and_log_index[log_key]
+            inconsistencies.append(LogInconsistency(
+                tx_hash=failed_tx_hash,
+                log_index=int(getlogs_log['logIndex'], 16),
+                block_number=int(getlogs_log['blockNumber'], 16),
+                type='log from failed tx found in eth_getLogs',
+                receipt_log=None,
+                getlogs_log=getlogs_log
+            ))
+            del logs_by_tx_hash_and_log_index[log_key]
+
+    # Nothing leftover should be here
+    for log_key, getlogs_log in logs_by_tx_hash_and_log_index.items():
+        print(f"INCONSISTENCY! Log {log_key} found from eth_getLogs but not from eth_getTransactionReceipt")
+        inconsistencies.append(LogInconsistency(
+            tx_hash=getlogs_log['transactionHash'],
+            log_index=int(getlogs_log['logIndex'], 16),
+            block_number=int(getlogs_log['blockNumber'], 16),
+            type='log missing from eth_getTransactionReceipt',
+            receipt_log=None,
+            getlogs_log=getlogs_log
+        ))
+
+    return inconsistencies
+
+
+def check_data_mismatch(receipt_log: Dict[str, Any], getlogs_log: Dict[str, Any]) -> Optional[LogInconsistency]:
     if receipt_log['data'] != getlogs_log['data']:
         return LogInconsistency(
             tx_hash=receipt_log['transactionHash'],
             log_index=int(receipt_log['logIndex'], 16),
             block_number=int(receipt_log['blockNumber'], 16),
+            type='data mismatch',
             receipt_log=receipt_log,
             getlogs_log=getlogs_log,
         )
     return None
+
+
+def print_percentage_progress(i, length):
+    if length >= 10:
+        if i % (length // 10) == 0:
+            print(i // (length // 10) * 10, '% ...')
 
 
 if __name__ == '__main__':
