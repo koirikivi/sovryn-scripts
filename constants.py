968bc41fe4a3b8bf0fad286eb7d9d7c84e5b35b6
Rainer Koirikivi <rainer@koirikivi.fi>
2022-09-18 08:51:29 +0300

    Update federator addresses

--- constants.py
+++ constants.py
@@ -6,28 +6,32 @@ BRIDGES = {
     'rsk_eth_mainnet': {
         'rsk': {
             'bridge_address': '0x1ccad820b6d031b41c54f1f3da11c0d48b399581',
-            'federation_address': '0x5e2ee3cd18421838d066bd1dc02fb1f767d834dd',
-            'bridge_start_block': 3373266,
+            'federation_address': '0x32593e4f7a4991c2fe17459dae9920fd612855b4',
+            #'bridge_start_block': 3373266,
+            'bridge_start_block': 3600000,
             'chain': 'rsk_mainnet',
         },
         'other': {
             'bridge_address': '0x33c0d33a0d4312562ad622f91d12b0ac47366ee1',
-            'federation_address': '0x74aa9b461CAd174cA066fc80AF2151c96Bd4D45f',
-            'bridge_start_block': 12485023,
+            'federation_address': '0xD77b76A65a19715BDcB5eE223928af2919836A3E',
+            #'bridge_start_block': 12485023,
+            'bridge_start_block': 13100000,
             'chain': 'eth_mainnet',
         }
     },
     'rsk_bsc_mainnet': {
         'rsk': {
             'bridge_address': '0x971b97c8cc82e7d27bc467c2dc3f219c6ee2e350',
-            'federation_address': '0xa7938270c2bf99a9cf033c9d35156d2ddb097b12',
-            'bridge_start_block': 3399221,
+            'federation_address': '0xD1E45F51C8f09B139218FC75D26409096316971C',
+            #'bridge_start_block': 3399221,
+            'bridge_start_block': 3600000,
             'chain': 'rsk_mainnet',
         },
         'other': {
             'bridge_address': '0xdfc7127593c8af1a17146893f10e08528f4c2aa7',
-            'federation_address': '0x33f8743d5afa519dd373a75e7aa5c255e2c82468',
-            'bridge_start_block': 7917126,
+            'federation_address': '0x502fBCe27973d4bE1E69a4099046762251D005B4',
+            #'bridge_start_block': 7917126,
+            'bridge_start_block': 10300000,
             'chain': 'bsc_mainnet',
         }
     },
@@ -48,15 +52,13 @@ BRIDGES = {
     'rsk_bsc_testnet': {
         'rsk': {
             'bridge_address': '0x2b2bcad081fa773dc655361d1bb30577caa556f8',
-            #'federation_address': '0x92f791b72842f479888aefba975eff2ed74700b7',
-            'federation_address': '0x07081144a97b58f08AB8bAaf8b05D87f5d31e5dF',
+            'federation_address': '0x92f791b72842f479888aefba975eff2ed74700b7',
             'bridge_start_block': 1884421,
             'chain': 'rsk_testnet',
         },
         'other': {
             'bridge_address': '0x862e8aff917319594cc7faaae5350d21196c086f',
-            #'federation_address': '0x2b456e230225c4670fbf10b9da506c019a24cac7',
-            'federation_address': '0x6E28bB6dbBAc8bBC11F5780E39f9Aca9F9737182',
+            'federation_address': '0x2b456e230225c4670fbf10b9da506c019a24cac7',
             'bridge_start_block': 9290364,
             'chain': 'bsc_testnet',
         }
